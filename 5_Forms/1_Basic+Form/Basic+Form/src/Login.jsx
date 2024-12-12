dbdea78440dd7b73751feb74ca742cce7fbf5c5c
mangesh.hagare22 <mangesh.hagare20@gmail.com>
2024-12-12 20:03:57 +0530

    created login form component with email and password fields - Implemented state binding for form inputs - Added basic styling using Bootstrap

---
+++ 5_Forms/1_Basic+Form/Basic+Form/src/Login.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { email: "abc@test.com", password: "abc123" };
+  }
+
+  render() {
+    return (
+      <div className="col-lg-9">
+        <h4 className="m-1 p-2 border-bottom">Login</h4>
+
+        {/* Email starts */}
+        <div className="form-group form-row">
+          <label className="col-lg-4">Email:</label>
+          <input
+            type="text"
+            className="form-control"
+            value={this.state.email}
+          />
+        </div>
+        {/* Email ends */}
+
+        {/* Password starts */}
+        <div className="form-group form-row">
+          <label className="col-lg-4">Password:</label>
+          <input
+            type="password"
+            className="form-control"
+            value={this.state.password}
+          />
+        </div>
+        {/* Password ends */}
+      </div>
+    );
+  }
+}
